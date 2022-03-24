e037e85a6576de5e098323755efea5b136cbb2d6
João Rios <101368524+joaor1os@users.noreply.github.com>
2022-03-24 19:23:35 +0000

    new

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ app = Flask('__main__')
 def index():
     return render_template('index.html')
 
-
 @app.route('/unifran')
 def unifran():
     return render_template('unifran.html')
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0')
