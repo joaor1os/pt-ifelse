37df7db646dfa2f955ea489ddaa325a1b5947052
joaor1os <101368524+joaor1os@users.noreply.github.com>
2022-03-24 19:19:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask('__main__')
+
+
+@app.route('/')
+def index():
+    return render_template('/index.html')
+
+
+@app.route('/unifran')
+def unifran():
+    return render_template('/unifran.html')
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
